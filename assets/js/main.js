04491f4ca7b64df0d4d394c847f3f3317cde0dbe
Maciej Wrzesinski <maciej.wrzesinski.95@gmail.com>
2019-01-21 10:44:36 +0100

    Done some MTF

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,7 +185,7 @@ const uploadClick = function () {
             $('#file_name').text('File Name: ' + file.name);
             $('#file_size').text('Size: ' + file.size + ' bytes');
             $('#text').text(unicodeString.substring(0, ($(window).width() + $(window).height())) + '...');
-            uploadedString = unicodeString;
+            //uploadedString = unicodeString;
         };
 
         reader.readAsArrayBuffer(file);
@@ -240,11 +240,14 @@ $().ready(function() {
     triggerPageRotation();
 
     const bzip2 = new BZIP2();
-    let compressedText = bzip2.compress('disa sisa hi');
-    let decompressedText = bzip2.decompress('aassi hsdii ');
-    console.log('disa sisa hi');
+    let text = 'this is a very nice conversation <>?::"{}][;/., !@#$%^&*())_+  +_)(*&^%$#@! 1234567890-= =-0987654321 Z!x2C#v4B%n6M&pomoc7557 Hello :)  ';
+    let compressedText = bzip2.compress(text);
+    let decompressedText = bzip2.decompress(compressedText);
+    console.log('-------------------------')
     console.log(compressedText);
     console.log(decompressedText);
+
+    console.log(text == decompressedText)
 });
 
 /* BZIP2 algorithm */
@@ -252,9 +255,17 @@ $().ready(function() {
 function BZIP2() {
     var that = this;
     this.originalStartingIndex = 0;
+    this.maxChar = 0;
 
     /* usable methods */
+
     this.compress = function(dataString) {
+        //get max char so we can limit char buckets
+        for (let i = 0; i < dataString.length; i++)
+            if (dataString[i].charCodeAt(0) > this.maxChar)
+                this.maxChar = dataString[i].charCodeAt(0);
+        this.maxChar++;
+
         return doHuffmans(doMoveToFront(doBurrowsWheelerTransform(dataString)));
     }
 
@@ -263,13 +274,14 @@ function BZIP2() {
     }
 
     /* private methods that private methods below use */
-    let sortWithIndeces = function (originalArray) {
-        let toSort = originalArray.slice();
-        let size = toSort.length;
 
-        //add indeces
-        for (let i = 0; i < toSort.length; i++) {
-            toSort[i] = [toSort[i], i];
+    let sortWithIndeces = function (originalArray) {
+        let size = originalArray.length;
+        let toSort = new Array(size);
+
+        //add indeces and arrange it this way so .sort() can work with this properly
+        for (let i = 0; i < originalArray.length; i++) {
+            toSort[i] = [originalArray.slice().splice(i, originalArray.length).join('') + originalArray.join(''), i];
         }
 
         //sort them out
@@ -277,58 +289,35 @@ function BZIP2() {
             return left[0] < right[0] ? -1 : 1;
         });
 
-        //sort the same chars by next chars in the string
-        let i = 0;
-        let debug = 0
-        while (i < size - 1) {
-            if (toSort[i][0] == toSort[i+1][0]) {
-
-                let j = 1;
-                let checked = 0;
-                //this loop checks every following char, stop on the end of the string
-                while (checked === 0 && j+i < size - 1 && debug < 1000 ) {
-                    debug++;
-
-                    if (toSort[i][1] + j === size || toSort[i + 1][1] + j === size) { //if it got to the end of string, then end the loop it does not have to swap :)
-                        checked = 1;
-                        continue;
-                    }
-                    let firstIndex = (toSort[i][1] + j);
-                    let secondIndex = (toSort[i + 1][1] + j);
-
-                    //this only checks one letter after the thing MAKE IT TO CHECK ALL OF THEM
-                    if (originalArray[firstIndex] > originalArray[secondIndex]) {
-                        let x = toSort[i][0], y = toSort[i][1];
-                        toSort[i][0] = toSort[i + 1][0];
-                        toSort[i][1] = toSort[i + 1][1];
-                        toSort[i + 1][0] = x;
-                        toSort[i + 1][1] = y;
-
-                        //go back 2 (actually 1) step back to check the letter before
-                        i = 0;
-                        checked = 1;
-                    }
-                    //if the following chars are still the same, check next ones
-                    else if (originalArray[firstIndex] === originalArray[secondIndex]) {
-                        j++;
-                    }
-                    else {
-                        checked = 1;
-                    }
-                }
-            }
-            i++;
-        }
-
         toSort.sortIndices = [];
         for (let j = 0; j < toSort.length; j++) {
             toSort.sortIndices.push(toSort[j][1]);
             toSort[j] = toSort[j][0];
         }
+
         return toSort;
     };
 
+    let sortAlphabetically2DArray = function (array, size, sortedArrayNumber) {
+        let i = 0;
+        while (i !== size) {
+            if (array[sortedArrayNumber][i] > array[sortedArrayNumber][i + 1]) {
+                for (let j = 0; j <= sortedArrayNumber; j++) {
+                    if (array[j][i]) {
+                        //swap elements in array
+                        array[j][i] = array[j].splice(i + 1, 1, array[j][i])[0];
+                    }
+                }
+                --i;
+            }
+            else
+                ++i;
+        }
+        return array;
+    };
+
     /* private methods */
+
     let doBurrowsWheelerTransform = function(dataString) {
 
         let size = dataString.length;
@@ -337,7 +326,6 @@ function BZIP2() {
 
         transformedString = sortWithIndeces(dataString.slice());
 
-        console.log(transformedString)
         //find the string 'sorted text - 1'
         let finalText = [];
         for (let i = 0; i < size; i++) {
@@ -351,6 +339,7 @@ function BZIP2() {
                 break;
             }
         }
+
         return finalText.join('');
     }
 
@@ -382,11 +371,58 @@ function BZIP2() {
     }
 
     let doMoveToFront = function(dataString) {
-        return dataString;
+        let size = dataString.length;
+        dataString = dataString.split('');
+        finalString = new Array(size);
+        let charsArray = new Array(that.maxChar);
+
+        for (let i = 0; i < that.maxChar; i++) {
+            charsArray[i] = i;
+        }
+
+        for (let i = 0; i < size; i++) {
+
+            let chosenIndex = 0;
+            for (let j = 0; j < that.maxChar; j++) {
+                if (dataString[i].charCodeAt(0) == charsArray[j]) {
+                    chosenIndex = j;
+                    break;
+                }
+            }
+            finalString[i] = chosenIndex;
+
+            charsArray.unshift(parseInt(charsArray.splice(chosenIndex, 1).join()));
+        }
+        return finalString;
     }
 
     let undoMoveToFront = function(dataString) {
-        return dataString;
+        let size = dataString.length;
+        finalString = new Array(size);
+        let charsArray = new Array(that.maxChar);
+
+        for (let i = 0; i < that.maxChar; i++) {
+            charsArray[i] = i;
+        }
+
+        for (let i = 0; i < size; i++) {
+
+            finalString[i] = charsArray[dataString[i]];
+            let tmp = charsArray[dataString[i]];
+
+            for (let j = dataString[i]; j > 0 ; j--) {
+                charsArray[j] = charsArray[j-1];
+            }
+
+            charsArray[0] = tmp;
+        }
+
+        //convert back to string
+        for (let i = 0; i < size; i++) {
+            finalString[i] = String.fromCharCode(finalString[i]);
+        }
+
+        return finalString.join('');
     }
 
     let doHuffmans = function(dataString) {
@@ -397,113 +433,3 @@ function BZIP2() {
         return dataString;
     }
 }
-
-var uploadedString = 0;
-
-var transformedText;
-
-const swapElementsArray = function (textArray, index1, index2) { //this swaps elements in array
-    let a = textArray[index1];
-    textArray[index1] = textArray[index2];
-    textArray[index2] = a;
-    //textArray[index1] = textArray.splice(index2, 1, textArray[index1])[0];
-};
-
-const sortAlphabetically2DArray = function (array, size, sortedArrayNumber) {
-    let i = 0;
-    while (i !== size) {
-        if (array[sortedArrayNumber][i] > array[sortedArrayNumber][i + 1]) {
-            //console.log(array[sortedArrayNumber][i] + ":" + array[sortedArrayNumber][i].charCodeAt(0) + " > " + array[sortedArrayNumber][i+1] + ":" + array[sortedArrayNumber][i+1].charCodeAt(0))
-            for (let j = 0; j <= sortedArrayNumber; j++) {
-                if (array[j][i]) {
-                    swapElementsArray(array[j], i, i + 1);
-                }
-            }
-            --i;
-        }
-        else
-            ++i;
-    }
-    return array;
-};
-
-const decompressBWT = function (compressedText, originalIndex) {
-    let size = dataString.length;
-    dataString = dataString.split('');
-    let bucket = new Array(256).fill(0);
-    let F = [];
-    let indices = new Array(size);
-    let finalText = new Array(size).fill(0);
-
-    //fill the bucket with count of each char
-    for (let i = 0; i < size; i++)
-        bucket[dataString[i].charCodeAt(0)]++;
-
-    //F now has chars in increasing order
-    for (let i = 0; i < 256; i++)
-        for (let j = 0; j < bucket[i]; ++j)
-            F.push(i);
-
-    for (let i = 0, j = 0; i < 256; i++) {
-        while (i >= F[j] && j < size)
-            ++j;
-
-        bucket[i] = j;
-    }
-
-    ///this does somethig fucky
-    for(let i = 0; i < size; i++) {
-        console.log("W " + dataString[i].charCodeAt(0) + " jest " + bucket[dataString[i].charCodeAt(0)] + " ma byc " + i)
-
-        indices[bucket[dataString[i].charCodeAt(0)]++] = i;
-    }
-
-    console.log(indices);
-
-    let j = that.originalStartingIndex;
-    for(let i = 0; i < size; ++i) {
-        console.log(dataString[j]);
-        finalText[i] = dataString[j];
-        j=indices[j];
-    }
-
-    return finalText.join('');
-};
-
-const decompressBWT2 = function (compressedText, originalIndex) {
-    let size = compressedText.length;
-    compressedText = compressedText.split('');
-    let theArray = new Array(size).fill('');
-
-
-
-    theArray[0] = compressedText.slice();
-    theArray = sortAlphabetically2DArray(theArray.slice(), size, 0);
-    for (let i = 1; i < size; i++) {
-        theArray[i] = new Array(size);
-    }
-    console.log(theArray.slice());
-
-    theArray[1] = compressedText.slice();
-    theArray = sortAlphabetically2DArray(theArray.slice(), size, 1);
-    console.log(theArray.slice());
-
-    theArray[2] = compressedText.slice();
-    theArray = sortAlphabetically2DArray(theArray.slice(), size, 2);
-
-    console.log(theArray.slice());
-    /*for (let i = 1; i < 3; i++) {
-        theArray[i] = compressedText.slice();
-        theArray = sortAlphabetically2DArray(theArray.slice(), size, i);
-        console.log(theArray.slice());
-    }*/
-
-    let finalText = new Array(size).fill('');
-    for (let i = 0, j = size; i < size; i++, j--) {
-        finalText[j] = theArray[i][originalIndex];
-    }
-
-    return finalText.join('');
-};
-
-
